789d89d217a0ecad5310e6e7fae054753ba272ef
trdmonkey <trdmonkey218@gmail.com>
2025-09-27 22:49:29 -0500

    diseño index x2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,7 +30,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" 
                              viewBox="0 0 24 24" stroke="currentColor">
                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                                   d="M12 20l9-15-9-5-9 5 9 5z" />
+                                   d="M12 20l9-5-9-5-9 5 9 5z" />
                         </svg>
                     </div>
                     <h2 class="text-xl font-semibold text-slate-800">{{ $post->title }}</h2>
